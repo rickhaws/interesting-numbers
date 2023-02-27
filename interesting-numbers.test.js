d3bf72a1c79fc3380bbd9cde2976d78473ad8ead
Rick Haws <61323632+rickhaws@users.noreply.github.com>
2023-02-27 09:57:38 -0700

    Create interesting-numbers.test.js

---
+++ interesting-numbers.test.js
@@ -0,0 +1,27 @@
+// Since Node 10, we're using Mocha.
+// You can use `chai` for assertions.
+const chai = require("chai");
+const assert = chai.assert;
+// Uncomment the following line to disable truncating failure messages for deep equals, do:
+// chai.config.truncateThreshold = 0;
+// Since Node 12, we no longer include assertions from our deprecated custom test framework by default.
+// Uncomment the following to use the old assertions:
+// const Test = require("@codewars/test-compat");
+
+describe("Solution", function() {
+  it("should test for something", function() {
+    Test.assertEquals(isInteresting(99, [10000012]), 1);
+    Test.assertEquals(isInteresting(98, [10000012]), 1);
+    Test.assertEquals(isInteresting(100, [10000012]), 2);
+    Test.assertEquals(isInteresting(999, [10000012]), 2);
+    Test.assertEquals(isInteresting(899, [10000012]), 1);
+    Test.assertEquals(isInteresting(898, [10000012]), 1);
+    Test.assertEquals(isInteresting(89998, [10000012]), 2);
+    Test.assertEquals(isInteresting(1337, [1337, 31415, 314159, 24680]), 2);
+    Test.assertEquals(isInteresting(31414, [1337, 31415, 314159, 24680]), 1);
+    Test.assertEquals(isInteresting(31413, [1337, 31415, 314159, 24680]), 2);
+    Test.assertEquals(isInteresting(1338, [1337, 31415, 314159, 24680]), 0);
+    Test.assertEquals(isInteresting(2477, [1337, 31415, 314159, 24680]), 0);
+    Test.assertEquals(isInteresting(24681, [1337, 31415, 314159, 24680]), 0);
+  });
+});
