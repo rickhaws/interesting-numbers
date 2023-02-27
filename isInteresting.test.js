28e9b6db0b34c4541faef37e1e8b3efee4721b5c
Rick Haws <61323632+rickhaws@users.noreply.github.com>
2023-02-27 11:25:10 -0700

    Corrected and updated tests

--- isInteresting.test.js
+++ isInteresting.test.js
@@ -17,7 +17,7 @@ describe('Basic inputs', function() {
     Test.assertEquals(isInteresting(100, [10000012]), 2);
     Test.assertEquals(isInteresting(999, [10000012]), 2);
     Test.assertEquals(isInteresting(899, [10000012]), 1);
-    Test.assertEquals(isInteresting(898, [10000012]), 1);
+    Test.assertEquals(isInteresting(898, [10000012]), 2);
     Test.assertEquals(isInteresting(89998, [10000012]), 2);
     Test.assertEquals(isInteresting(1337, [1337, 31415, 314159, 24680]), 2);
     Test.assertEquals(isInteresting(31414, [1337, 31415, 314159, 24680]), 1);
@@ -25,6 +25,8 @@ describe('Basic inputs', function() {
     Test.assertEquals(isInteresting(1338, [1337, 31415, 314159, 24680]), 0);
     Test.assertEquals(isInteresting(2477, [1337, 31415, 314159, 24680]), 0);
     Test.assertEquals(isInteresting(24681, [1337, 31415, 314159, 24680]), 0);
+    Test.assertEquals(isInteresting(23456, [1337, 31415, 314159, 24680]), 2);
+    Test.assertEquals(isInteresting(543210, [1337, 31415, 314159, 24680]), 2);
     Test.assertEquals(isInteresting(3, [1337, 256]),     0);
     Test.assertEquals(isInteresting(98, [1337, 256]),    1);
     Test.assertEquals(isInteresting(3, [1337, 256]),     0);
@@ -34,5 +36,4 @@ describe('Basic inputs', function() {
     Test.assertEquals(isInteresting(11209, [1337, 256]), 1);
     Test.assertEquals(isInteresting(11211, [1337, 256]), 2);
   });
-});  });
 });
